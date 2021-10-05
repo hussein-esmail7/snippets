e32a9be48170f4ba64f597de918d1a78c33a3d69
Hussein Esmail <hussein.esmail7@gmail.com>
2021-10-04 21:07:36 -0400

    modified file

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # snippets
-Code Snippets
+Hello! This repository is mainly for myself for files that I can refer to (though you can still use them if you want to).
+
+# Table of contents
+- [Table of Contents](#table-of-contents)
+- [File Descriptions](#File-Descriptions)
+	- [[C] rw_files.c](#rw_files.c)
+- [Donate](#donate)
+
+# File Descriptions
+
+## rw_files.c
+This snippet is how to read and write files in C.
+
+
+# Donate
+[!["Buy Me A Coffee"](https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png)](https://www.buymeacoffee.com/husseinesmail)
---
+++ rw_files.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/*
+c_write.c
+Hussein Esmail
+Created: 2021 10 04
+Updated: 2021 10 04
+Terminal command to compile file to an executable:
+    gcc -o c_write c_write.c && chmod +x c_write && ./c_write
+Description: This is a code snippet on how to read and write to and from files.
+*/
+
+int main(void) {
+
+    char fileName[] = "test.txt";
+    char fileWrite[] = "This is what is in the text file\n";
+
+    // WRITING 
+    FILE* fpointer = fopen(fileName, "w");  // File pointer with fopen (file name, file mode (r/w/a))
+    // Writing and appending is used the same way.
+    // fprintf(file pointer, String to write);
+    // This does not put the endline character, you have to add that like in C
+    fprintf(fpointer, fileWrite);
+    fclose(fpointer); // This must always be here after you're done with a file
+
+    // READING 
+    int line_size = 255;
+    char line[line_size]; // String will be stored here
+    FILE* fpointer1 = fopen(fileName, "r");  // File pointer with fopen (file name, file mode (r/w/a))
+    fgets(line, line_size, fpointer1); // fgets(where to store info, max size to read. Should be same as the char[] size, file pointer);
+    // This only gets 1 line, but the first line_size characters. To get the next line, you have to run fgets() again. This can be looped.
+    printf("%s", line);
+    fclose(fpointer1); // This must always be here after you're done with a file
+
+
+    return 0;
+}
